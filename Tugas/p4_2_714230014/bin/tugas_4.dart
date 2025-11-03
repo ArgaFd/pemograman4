988a573227a8817bcdeb8ce6904e05f974853a5f
ArgaFd <bagusarga3@gmail.com>
2025-11-03 20:11:11 +0700

    Remove all files

--- Tugas/p4_2_714230014/bin/tugas_4.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tugas_4/tugas_4.dart' as tugas_4;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tugas_4.calculate()}!');
-}
--- Tugas/p4_2_714230014/test/tugas_4_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tugas_4/tugas_4.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
